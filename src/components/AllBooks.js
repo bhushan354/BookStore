d2ad533f48b3ad31e9ca2ff02f32f2c479e5d2fd
bhushan354 <deshmukhbhushan354@gmail.com>
2023-09-30 05:08:30 +0530

    Adding Books Using for loop instead of map

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -3,20 +3,23 @@ import { useSelector } from 'react-redux';
 import Book from './Book';
 
 function AllBooks() {
-  const allBook = useSelector((state) => state.book.allBook);
+  const allBooks = useSelector((state) => state.book.allBook);
 
-  return (
-    <>
-      {allBook.map((book) => (
-        <Book
-          key={book.item_id}
-          title={book.title}
-          author={book.author}
-          item_id={book.item_id}
-        />
-      ))}
-    </>
-  );
+  const bookComponents = [];
+
+  for (let i = 0; i < allBooks.length; i += 1) {
+    const book = allBooks[i];
+    bookComponents.push(
+      <Book
+        key={book.item_id}
+        title={book.title}
+        author={book.author}
+        item_id={book.item_id}
+      />,
+    );
+  }
+
+  return <>{bookComponents}</>;
 }
 
 export default AllBooks;
